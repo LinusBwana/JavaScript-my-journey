097177c4a7bbdd331d609ed64f798cbc14d7f1db
LinusBwana <linusbwana49@gmail.com>
2025-06-10 11:20:47 +0300

    learning Math object

---
+++ math object/index.js
@@ -0,0 +1,35 @@
+// Math - built in object that provides a collection of properties and methods
+
+// example1
+/*
+let x = 3.321;
+let y = 2;
+let z = 1;
+
+z = Math.round(x);
+z = Math.floor(x);
+z = Math.ceil(x);
+z = Math.abs(x);
+z = Math.trunc(x);
+z = Math.pow(x,y)
+z = Math.sqrt(x);
+z = Math.log(x);
+z = Math.sin(x);
+z = Math.tan(x);
+
+console.log(z);
+*/
+
+
+// example 2
+let x = 3.321;
+let y = 2;
+let z = 1;
+
+let max = Math.max(x,y,z);
+
+let min = Math.min(x,y,z);
+
+console.log(max)
+
+
